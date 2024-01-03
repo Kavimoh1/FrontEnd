1a3aa3420a61a10f067168d0588557b5bf742bb0
Kavimoh1 <mohan.j.ghogarkar@gmail.com>
2024-01-04 02:06:00 +0530

    Function/Class Component and increment

--- state-optimization/src/App.jsx
+++ state-optimization/src/App.jsx
@@ -3,14 +3,15 @@ import './App.css';
 import React, { useState } from 'react';
 // function component
 function App() {
-  // This will not increment value in UI but It will increment value in console. To render value in UI we need to rerender ther component . for that we need to use {states}
- // let counter =0; insted of this we need to use useState hook. In useState we need to provide initial value
+//   This will not increment value in UI but It will increment value in console. To render value in UI we need to rerender ther component . for that we need to use {states}
+//  let counter =0; insted of this we need to use useState hook. In useState we need to provide initial value
+console.log("component render");
  let [counter, setCounter]=useState(0);
+ 
   const incrementFunction=()=>{
-    counter=counter+1;
-    console.log(counter); 
-
+    setCounter(counter+1);
   };
+
   return (
   //  to use javascript component inside JSX code -use {}
    <div class="container">
@@ -20,14 +21,25 @@ function App() {
   );
 }
 
-// // class component
-// // do not add bracket at end like- 
-// // class App2 extends React.Component()
+// class component
+// do not add bracket at end like- 
+// class App2 extends React.Component()
 
-// class App2 extends React.Component {
-//   render(){
-//     return (
-//    <h1>Class Component</h1>
-//   );
-// }}
+// class App extends React.Component {
+// // state is a object in class component
+//   state={
+//     counter:0
+//   }
+//  incrementFunction=()=>{
+//    this.setState({counter:this.state.counter+1})
+//   }
+//   render() {
+//     console.log("component render");  
+//     return(
+//   //  to use javascript component inside JSX code -use {}
+//    <div className="container">
+//     <h1>Counter:{this.state.counter}</h1>
+//     <button className="btn btn-primary" onClick={this.incrementFunction}>Increment</button>
+//    </div>
+//   )}}
 export default App;
